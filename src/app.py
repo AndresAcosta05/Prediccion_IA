b7b66be159f1872b0798c43a4ee3ab252e2e8776
Samir Rojas <samirrojas42@gmail.com>
2023-05-03 21:49:04 -0500

    Version 1.3

--- src/app.py
+++ src/app.py
@@ -16,6 +16,31 @@ def index():
 def Nosotros():
     return render_template('Nosotros.html')
 
+@app.route('/Prediccion')
+def Prediccion():
+    return render_template('Prediccion.html')
+
+@app.route('/Reporte')
+def Reporte():
+    return render_template('Reporte.html')
+
+
+@app.route('/Gestion')
+def Gestion():
+    return render_template('Gestion.html')
+
+
+@app.route('/Redneuronal')
+def Redneuronal():
+    return render_template('Redneuronal.html')
+
+@app.route('/Contactenos')
+def Contactenos():
+    return render_template('Contactenos.html')
+
+
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     # check if want the login
